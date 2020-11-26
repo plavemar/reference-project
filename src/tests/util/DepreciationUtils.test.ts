4985f9728409c403c3a96125c7757412a1207f42
Martin Plavek <plavemar@gmail.com>
2020-11-26 08:27:06 +0100

    Depreciation Calc - DepreciationUtils unit tests

--- src/tests/util/DepreciationUtils.test.ts
+++ src/tests/util/DepreciationUtils.test.ts
@@ -3,23 +3,15 @@ import {calculateDepreciation, calculateRemainingValue} from "../../util/Depreci
 
 
 test("calculateDepreciationTest", () => {
-    const purchaseValue: number = 920000;
     const depreciationGroup: DepreciationGroup = {
         firstYear: 5,
         nextYears: 6,
         afterEvaluation: 5
     };
 
-    expect(calculateDepreciation(1, purchaseValue, false, depreciationGroup, 0)).toBe(184000);
+    expect(calculateDepreciation(1, 920000, false, depreciationGroup, 0)).toBe(184000);
 });
 
 test("calculateRemainingValueTest", () => {
-    const purchaseValue: number = 920000;
-    const depreciationGroup: DepreciationGroup = {
-        firstYear: 5,
-        nextYears: 6,
-        afterEvaluation: 5
-    };
-
     expect(calculateRemainingValue(920000, 184000)).toBe(736000);
 });
